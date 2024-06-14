1ccfd739990f7c9d838fe85c271db193d125ee19
nikhil4555 <nikhil.20bce7200@vitap.ac.in>
2024-06-14 17:21:24 +0530

    krtechno-hallticket

--- src/db/conn.js
+++ src/db/conn.js
@@ -9,7 +9,9 @@ const mongouri = process.env.MONGO_URI;
 console.log(mongouri);
 const connectDb = async () => {
   try {
-    await mongoose.connect(mongouri);
+    await mongoose.connect(mongouri,{
+      serverSelectionTimeoutMS: 10000  // Increased timeout to 20 seconds
+    });
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error(err);
