8ddea4ea02d4f0b469d741c30e03ea20377b0e09
nikhil4555 <nikhil.20bce7200@vitap.ac.in>
2024-06-15 11:02:32 +0530

    conn

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,18 +1,17 @@
-// db/conn.js
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-dotenv.config({path:'src/.env'})
 
-
+dotenv.config({ path: 'src/.env' }); // Assuming your .env file is in src folder
 
 const mongouri = process.env.MONGO_URI;
-console.log(mongouri);
+
 const connectDb = async () => {
   try {
-    await mongoose.connect(mongouri);
+    await mongoose.connect(mongouri, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error(err);
+    process.exit(1); // Exit the process on connection failure
   }
 };
 
